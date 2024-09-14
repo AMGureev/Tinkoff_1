91bb73c66af12b47f4900b00decd6be9419f0997
alexgureev <amgureev@edu.hse.ru>
2024-09-14 20:41:19 +0300

    feat: added StatisticsView.java
    Added 4 functions:
    displayStatistics();

---
+++ src/main/java/backend/academy/hangman/View/StatisticsView.java
@@ -0,0 +1,10 @@
+package backend.academy.hangman.View;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class StatisticsView {
+    public void displayStatistics(String statistics) {
+        System.out.println("Statistics:\n" + statistics);
+    }
+}
