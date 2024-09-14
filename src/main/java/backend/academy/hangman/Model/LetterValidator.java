977b43f1dcce850a59fcf80eec7237820f44c2c0
alexgureev <amgureev@edu.hse.ru>
2024-09-14 19:58:37 +0300

    fix: translate language Russian -> English

--- src/main/java/backend/academy/hangman/Model/LetterValidator.java
+++ src/main/java/backend/academy/hangman/Model/LetterValidator.java
@@ -5,12 +5,14 @@ import backend.academy.hangman.Entity.ValidatorErrorEntity;
 public class LetterValidator implements Validator {
     @Override
     public ValidatorErrorEntity isValid(String input) {
+        if (input.isEmpty()) {
+            return new ValidatorErrorEntity("Error: enter an integer.");
+        }
         if (input.length() == 1) {
             if (!Character.isLetter(input.charAt(0))) {
-                return new ValidatorErrorEntity("Ошибка: пользователь ввел не букву!");
+                return new ValidatorErrorEntity("Error: the user entered the wrong letter.");
             }
-            return null;
         }
-        return new ValidatorErrorEntity("Ошибка: пользователь ввел длинное слово!");
+        return null;
     }
 }
