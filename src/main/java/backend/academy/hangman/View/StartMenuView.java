073700fd2a71f4bb705f69d21f824a78e091c912
alexgureev <amgureev@edu.hse.ru>
2024-09-14 20:23:32 +0300

    feat: added StartMenuView.java
    Added 4 functions:
    displayStartMenu();
    displayGoodbye();
    displaySelect();
    displayError();

---
+++ src/main/java/backend/academy/hangman/View/StartMenuView.java
@@ -0,0 +1,24 @@
+package backend.academy.hangman.View;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class StartMenuView {
+    public void displayStartMenu() {
+        System.out.println("Welcome to Hangman!");
+    }
+    public void displayGoodbye() {
+        System.out.println("Goodbye!");
+    }
+
+    public void displaySelect() {
+        System.out.println("[1] Start Game");
+        System.out.println("[2] View Statistics");
+        System.out.println("[3] Exit");
+        System.out.print("Please select an option: ");
+    }
+
+    public void displayError() {
+        System.out.println("Error: please, input integer[1-3]");
+    }
+}
