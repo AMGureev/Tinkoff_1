6f677a167d828b6fdd5a90bc3d2e0f092dae0c16
alexgureev <amgureev@edu.hse.ru>
2024-09-14 20:48:00 +0300

    feat: added StatisticsModel.java
    Added 2 functions:
    tiString();
    addGame();

---
+++ src/main/java/backend/academy/hangman/Model/StatisticsModel.java
@@ -0,0 +1,29 @@
+package backend.academy.hangman.Model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class StatisticsModel {
+    private Integer countGame = 0;
+    private List<GameSession> gameSessions = new ArrayList<>();
+    @Override
+    public String toString() {
+        StringBuilder answer = new StringBuilder("Count game: " + countGame + "\n");
+        int counter = 1;
+        for (GameSession game : gameSessions) {
+            answer.append(counter++).append(". ").append(game.toString()).append("\n");
+        }
+        return answer.substring(0, answer.length() - 1);
+    }
+
+    public void addGame(GameSession gameSession) {
+        this.countGame += 1;
+        gameSessions.add(gameSession);
+    }
+}
