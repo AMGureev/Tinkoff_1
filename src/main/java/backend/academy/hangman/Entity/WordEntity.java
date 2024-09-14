38c55f5455f4d9c0aba6bfd352b4d9d0e5ad8fdc
alexgureev <amgureev@edu.hse.ru>
2024-09-14 20:01:12 +0300

    feat: added function toString() in WordEntity.java

--- src/main/java/backend/academy/hangman/Entity/WordEntity.java
+++ src/main/java/backend/academy/hangman/Entity/WordEntity.java
@@ -1,10 +1,16 @@
 package backend.academy.hangman.Entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class WordEntity {
     private String word;
     private String type;
     private String hint;
+    @Override
+    public String toString() {
+        return "Word: " + word + "; Type: " + type;
+    }
 }
