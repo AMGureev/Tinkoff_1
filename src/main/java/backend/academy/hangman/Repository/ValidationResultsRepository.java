f362578d5912f2d5a53e88f706f4b0f92e764a31
alexgureev <amgureev@edu.hse.ru>
2024-09-14 20:02:17 +0300

    feat: added function getLast() in ValidationResultsRepository.java (return ValidatorErrorEntity)

--- src/main/java/backend/academy/hangman/Repository/ValidationResultsRepository.java
+++ src/main/java/backend/academy/hangman/Repository/ValidationResultsRepository.java
@@ -3,5 +3,6 @@ package backend.academy.hangman.Repository;
 import backend.academy.hangman.Entity.ValidatorErrorEntity;
 
 public interface ValidationResultsRepository {
-    public void addResults(ValidatorErrorEntity validatorErrorEntity);
+    void addResults(ValidatorErrorEntity validatorErrorEntity);
+    ValidatorErrorEntity getLast();
 }
