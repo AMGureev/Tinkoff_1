6b9cc72fe1cd2f8cd23817dde5cc12fe23ca3b97
alexgureev <amgureev@edu.hse.ru>
2024-09-14 20:56:12 +0300

    feat: create SelectionStagesMenuController
    Added 2 functions:
    chooseCategory();
    chooseLevel();

---
+++ src/main/java/backend/academy/hangman/Controller/SelectionStagesMenuController.java
@@ -0,0 +1,45 @@
+package backend.academy.hangman.Controller;
+
+import backend.academy.Main;
+import backend.academy.hangman.Entity.WordEntity;
+import backend.academy.hangman.Model.SelectionCategoryMenu;
+import backend.academy.hangman.Model.StatisticsModel;
+import backend.academy.hangman.View.GameView;
+import backend.academy.hangman.View.SelectionCategoryMenuView;
+import com.google.inject.Inject;
+
+public class SelectionStagesMenuController {
+    private final SelectionCategoryMenu model;
+    private final SelectionCategoryMenuView view;
+
+    @Inject
+    public SelectionStagesMenuController(
+        SelectionCategoryMenu model,
+        SelectionCategoryMenuView view
+    ) {
+        this.model = model;
+        this.view = view;
+    }
+
+    public void chooseCategory() {
+        view.printHeading();
+        model.viewCategory();
+        int choice;
+        choice = model.input();
+        chooseLevel(choice);
+    }
+
+    public void chooseLevel(Integer choice) {
+        view.displaySetLevel();
+        int level = model.input();
+        WordEntity word = model.getRandomWordByCategoryAndLevel(choice, level);
+        if (word != null) {
+            GameController controller = new GameController(word, Main.injector.getInstance(StatisticsModel.class),
+                Main.injector.getInstance(GameView.class));
+            controller.startGame();
+        } else {
+            System.out.println("Error: there are no words of such complexity.");
+        }
+    }
+
+}
