e5cf8c900434d4e89ee7e1f8f455f295800b5622
alexgureev <amgureev@edu.hse.ru>
2024-09-14 21:00:55 +0300

    fix: changed Main.java

--- src/main/java/backend/academy/Main.java
+++ src/main/java/backend/academy/Main.java
@@ -1,9 +1,20 @@
 package backend.academy;
 
+import backend.academy.hangman.AppModule;
+import backend.academy.hangman.Controller.StartMenuController;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import lombok.Getter;
 import lombok.experimental.UtilityClass;
+
 
 @UtilityClass
 public class Main {
+    @Getter public static Injector injector;
     public static void main(String[] args) {
+        injector = Guice.createInjector(new AppModule());
+        StartMenuController controller = injector.getInstance(StartMenuController.class);
+        controller.start();
     }
+
 }
