e6a5fc441199d25ebaa9f1c00cdc44179d5c1d35
alexgureev <amgureev@edu.hse.ru>
2024-09-14 20:06:16 +0300

    feat: added inject logic in project
    Added AppModule (extends AbstractModule)

---
+++ src/main/java/backend/academy/hangman/AppModule.java
@@ -0,0 +1,28 @@
+package backend.academy.hangman;
+
+import backend.academy.hangman.Controller.GameStatisticsController;
+import backend.academy.hangman.Controller.SelectionStagesMenuController;
+import backend.academy.hangman.Model.SelectionCategoryMenu;
+import backend.academy.hangman.Model.StartMenu;
+import backend.academy.hangman.Model.StatisticsModel;
+import backend.academy.hangman.View.GameView;
+import backend.academy.hangman.View.SelectionCategoryMenuView;
+import backend.academy.hangman.View.StartMenuView;
+import backend.academy.hangman.View.StatisticsView;
+import com.google.inject.AbstractModule;
+import com.google.inject.Singleton;
+
+public class AppModule extends AbstractModule {
+    @Override
+    protected void configure() {
+        bind(StartMenuView.class).in(Singleton.class);
+        bind(StartMenu.class).in(Singleton.class);
+        bind(GameStatisticsController.class).in(Singleton.class);
+        bind(SelectionStagesMenuController.class).in(Singleton.class);
+        bind(SelectionCategoryMenu.class).in(Singleton.class);
+        bind(SelectionCategoryMenuView.class).in(Singleton.class);
+        bind(StatisticsModel.class).in(Singleton.class);
+        bind(StatisticsView.class).in(Singleton.class);
+        bind(GameView.class).in(Singleton.class);
+    }
+}
