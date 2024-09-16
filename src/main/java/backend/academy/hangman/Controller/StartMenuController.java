c07bca75f66d315a1afa964bcd9a0afd5c06eac7
AMGureev <amgureevhse@gmail.com>
2024-09-16 13:51:22 +0300

    fix: change magic numbers in StartMenuController

--- src/main/java/backend/academy/hangman/Controller/StartMenuController.java
+++ src/main/java/backend/academy/hangman/Controller/StartMenuController.java
@@ -9,7 +9,7 @@ public class StartMenuController {
     private final StartMenu model;
     private final GameStatisticsController statisticsController;
     private final SelectionStagesMenuController stagesMenu;
-    private final Integer MAX_COUNT;
+    private static final int DEFAULT_MAX_COUNT = 3;
 
     @Inject
     public StartMenuController(
@@ -22,13 +22,12 @@ public class StartMenuController {
         this.model = model;
         this.statisticsController = statisticsController;
         this.stagesMenu = stagesMenu;
-        MAX_COUNT = 3;
     }
 
     public void start() {
         view.displayStartMenu();
         int choice = 0;
-        while (choice != MAX_COUNT) {
+        while (choice != DEFAULT_MAX_COUNT) {
             view.displaySelect();
             choice = model.input();
             switch (choice) {
