1b946ad27c984ceff674e2f5a4c88f064081e0f9
AMGureev <amgureevhse@gmail.com>
2024-09-19 15:27:27 +0300

    fix: added Setter to injector in Main.java

--- src/main/java/backend/academy/Main.java
+++ src/main/java/backend/academy/Main.java
@@ -5,11 +5,12 @@ import backend.academy.hangman.Controller.StartMenuController;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class Main {
-    @Getter public static Injector injector = Guice.createInjector(new AppModule());
+    @Setter @Getter public static Injector injector = Guice.createInjector(new AppModule());
 
     public static void main(String[] args) {
         StartMenuController controller = injector.getInstance(StartMenuController.class);
