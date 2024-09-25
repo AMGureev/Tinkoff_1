31e1e3057608567da9a02aa77a6c12588d251538
alexgureev <amgureev@edu.hse.ru>
2024-09-25 14:03:08 +0300

    fix: added new class GameService
    Change logic in GameController

--- src/main/java/backend/academy/hangman/Controller/GameController.java
+++ src/main/java/backend/academy/hangman/Controller/GameController.java
@@ -1,143 +1,95 @@
 package backend.academy.hangman.Controller;
 
-import backend.academy.hangman.Entity.WordCollectorEntity;
-import backend.academy.hangman.Entity.WordEntity;
+import backend.academy.hangman.Entity.CommandInGameEnum;
+import backend.academy.hangman.Entity.ResultGameEnum;
+import backend.academy.hangman.Model.GameService;
 import backend.academy.hangman.Model.GameSession;
-import backend.academy.hangman.Model.HangmanStagesModel;
-import backend.academy.hangman.Model.StatisticsModel;
-import backend.academy.hangman.Model.ValidationResultsModel;
-import backend.academy.hangman.Model.WordCollectorModel;
 import backend.academy.hangman.View.GameView;
 import com.google.inject.Inject;
 import java.util.Objects;
 import java.util.Scanner;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 public class GameController {
-    private static final Logger LOGGER = LogManager.getLogger(GameController.class);
-    private HangmanStagesModel hangmanStageEntity;
-    private final ValidatorController validator = new ValidatorController(new ValidationResultsModel());
-    private final WordEntity wordToGuess;
-    private final WordCollectorModel wordCollectorModel = new WordCollectorModel(new WordCollectorEntity());
-    private final StatisticsModel statistics;
-    private final GameView view;
-    private boolean gameWon;
-    private boolean game = true;
+    private final GameService gameService;
+    private final GameView gameView;
 
     @Inject
     public GameController(
-        WordEntity wordToGuess,
-        StatisticsModel statistics,
-        GameView view
+        GameService gameService,
+        GameView gameView
     ) {
-        this.view = view;
-        this.hangmanStageEntity = HangmanStagesModel.STAGE_1;
-        this.wordToGuess = wordToGuess;
-        this.gameWon = false;
-        this.statistics = statistics;
+        this.gameService = gameService;
+        this.gameView = gameView;
     }
 
     public void startGame() {
-        view.greetingOutput();
-        view.printRemainingAttempts(hangmanStageEntity.getMaxAttempts());
-        GameSession gameSession = new GameSession(wordToGuess.word());
-        printWordType();
-        while (!Objects.equals(hangmanStageEntity.value(), HangmanStagesModel.STAGE_7.value()) && !gameWon && game) {
+        GameSession gameSession = new GameSession(gameService.wordToGuess().word());
+
+        gameView.greetingOutput();
+        gameView.printRemainingAttempts(gameService.hangmanStageModel().getMaxAttempts());
+        gameView.printWordType(gameService.wordToGuess().type());
+
+        while (!gameService.isGameOver()) {
             displayGameStatus();
-            String input = input().toLowerCase();
-            processInput(input);
-            addAttemptGameSession(gameSession);
-        }
-        if (!game) {
-            return;
-        } else {
-            if (gameWon) {
-                view.gameWonOutput(wordToGuess.word());
-                changeStatusGameSession(gameSession, "win");
-            } else {
-                printGallows();
-                view.gameLossOutput(wordToGuess.word());
-                changeStatusGameSession(gameSession, "defeat");
+            String userInput = input().toLowerCase();
+
+            boolean isIncorrectUserInput = gameService.isErrorThrown(userInput);
+            if (isIncorrectUserInput) {
+                gameView.printLastValidatorError(gameService.validator().getLast().message());
+            }
+
+            boolean isUserInputCommand = gameService.isCommand(userInput);
+            if (isUserInputCommand) {
+                processCommand(CommandInGameEnum.getCommandByString(userInput));
+            }
+
+            if (!isIncorrectUserInput && !isUserInputCommand) {
+                char userInputLetter = userInput.charAt(0);
+                if (gameService.checkingEnteredLetter(userInputLetter)) {
+                    gameView.printWarningReuseOfTheLetter();
+                } else {
+                    gameService.addLetterToWordCollectorModel(userInputLetter);
+                    gameService.checkingWhetherWordCollected(userInputLetter);
+                    addAttemptGameSession(gameSession);
+                }
             }
         }
-        statistics.addGame(gameSession);
+        if (!gameService.isActiveGame()) {
+            return;
+        }
+        if (Objects.equals(gameService.resultOfGame(), ResultGameEnum.WIN)) {
+            gameView.gameWonOutput(gameSession.word());
+        } else {
+            gameService.changeResultOfGame(ResultGameEnum.DEFEAT);
+            gameView.printGallows(gameService.hangmanStageModel());
+            gameView.gameLossOutput(gameSession.word());
+        }
+        gameService.changeStatusGameSession(gameSession);
+        gameService.addGameInStatistic(gameSession);
     }
 
-    private void processInput(String input) {
-        switch (input) {
-            case "get hint":
-                view.printHint(wordToGuess);
+    private void processCommand(CommandInGameEnum command) {
+        switch (command) {
+            case GET_HINT:
+                gameView.printHint(gameService.wordToGuess());
                 break;
 
-            case "exit":
+            case EXIT:
                 exit();
                 break;
 
-            case "menu":
-                view.returnOutput();
-                this.game = false;
-                break;
-
-            default:
-                if (!validator.checkInput(input)) {
-                    LOGGER.info(validator.getLast().message());
-                    break;
-                }
-                if (wordCollectorModel.getLetters().contains(input.charAt(0))) {
-                    view.printWarning();
-                    break;
-                }
-                wordCollectorModel.addLetter(input.charAt(0));
-                if (wordToGuess.word().contains(input)) {
-                    if (isWordGuessed()) {
-                        gameWon = true;
-                    }
-                } else {
-                    hangmanStageEntity = hangmanStageEntity.nextStage();
-                }
+            case MENU:
+                gameView.returnOutput();
+                gameService.changeActiveGameShutdown();
                 break;
         }
-    }
-
-    private boolean isWordGuessed() {
-        for (int i = 0; i < wordToGuess.word().length(); i++) {
-            if (!wordCollectorModel.getLetters().contains(wordToGuess.word().charAt(i))) {
-                return false;
-            }
-        }
-        return true;
     }
 
     private void displayGameStatus() {
-        printGallows();
-        LOGGER.info("Word: {}", getWordWithGuessedLetters());
-        LOGGER.info("Guessed letters: {}", wordCollectorModel.getLetters());
-        LOGGER.info("Enter a letter or a command ('get hint', 'exit', 'menu'):");
-    }
-
-    private String getWordWithGuessedLetters() {
-        StringBuilder wordBuilder = new StringBuilder();
-        for (int i = 0; i < wordToGuess.word().length(); i++) {
-            if (wordCollectorModel.getLetters().contains(wordToGuess.word().charAt(i))) {
-                wordBuilder.append(wordToGuess.word().charAt(i));
-            } else {
-                wordBuilder.append('_');
-            }
-        }
-        return wordBuilder.toString();
-    }
-
-    private void printGallows() {
-        LOGGER.info(hangmanStageEntity.value());
-    }
-
-    private void printWordType() {
-        view.printWordType(wordToGuess.type());
-    }
-
-    private void changeStatusGameSession(GameSession gameSession, String status) {
-        gameSession.status(status);
+        gameView.printGallows(gameService.hangmanStageModel());
+        gameView.printGameLineWithDashes(gameService.getWordWithGuessedLetters());
+        gameView.printListOfLettersUsed(gameService.wordCollectorModel().getLetters());
+        gameView.printAllCommands(gameService.getStringAboutAllCommands());
     }
 
     private void addAttemptGameSession(GameSession gameSession) {
@@ -145,7 +97,7 @@ public class GameController {
     }
 
     private void exit() {
-        view.goodbyeOutput();
+        gameView.goodbyeOutput();
         System.exit(0);
     }
 
---
+++ src/main/java/backend/academy/hangman/Model/GameService.java
@@ -0,0 +1,113 @@
+package backend.academy.hangman.Model;
+
+import backend.academy.hangman.Controller.ValidatorController;
+import backend.academy.hangman.Entity.CommandInGameEnum;
+import backend.academy.hangman.Entity.ResultGameEnum;
+import backend.academy.hangman.Entity.WordEntity;
+import com.google.inject.Inject;
+import lombok.Getter;
+
+import java.util.Objects;
+
+public class GameService {
+    @Getter
+    private final WordEntity wordToGuess;
+    private final StatisticsModel statistics;
+    @Getter
+    private ResultGameEnum resultOfGame;
+    @Getter
+    private HangmanStagesModel hangmanStageModel;
+    @Getter
+    private boolean isActiveGame = true;
+    @Getter
+    private final ValidatorController validator;
+    @Getter
+    private final WordCollectorModel wordCollectorModel;
+
+    @Inject
+    public GameService(WordEntity wordToGuess,
+                       StatisticsModel statistics,
+                       ValidatorController validator,
+                       WordCollectorModel wordCollectorModel) {
+        this.wordToGuess = wordToGuess;
+        this.statistics = statistics;
+        this.resultOfGame = ResultGameEnum.IN_PROGRESS;
+        this.hangmanStageModel = HangmanStagesModel.STAGE_1;
+        this.validator = validator;
+        this.wordCollectorModel = wordCollectorModel;
+    }
+
+    public boolean isGameOver() {
+        return (Objects.equals(hangmanStageModel.value(), HangmanStagesModel.STAGE_7.value())
+                || Objects.equals(resultOfGame.valueResult(), ResultGameEnum.WIN.valueResult())
+                || !isActiveGame);
+    }
+
+    public boolean isCommand(String inputString) {
+        return CommandInGameEnum.isCommandValid(inputString);
+    }
+
+    public boolean isErrorThrown(String inputString) {
+        return !validator.checkInput(inputString);
+    }
+
+    public boolean checkingEnteredLetter(char userLetter) {
+        return wordCollectorModel.getLetters().contains(userLetter);
+    }
+
+    public void addLetterToWordCollectorModel(char userLetter) {
+        wordCollectorModel.addLetter(userLetter);
+    }
+
+    public void checkingWhetherWordCollected(char userLetter) {
+        if (wordToGuess.word().contains(String.valueOf(userLetter))) {
+            if (isWordGuessed()) {
+                resultOfGame = ResultGameEnum.WIN;
+            }
+        } else {
+            hangmanStageModel = hangmanStageModel.nextStage();
+        }
+    }
+
+    private boolean isWordGuessed() {
+        for (int i = 0; i < wordToGuess.word().length(); i++) {
+            if (!wordCollectorModel.getLetters().contains(wordToGuess.word().charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void addGameInStatistic(GameSession gameSession) {
+        statistics.addGameInStatistic(gameSession);
+    }
+
+    public void changeStatusGameSession(GameSession gameSession) {
+        gameSession.status(resultOfGame.valueResult());
+    }
+
+    public void changeResultOfGame(ResultGameEnum resultOfGame) {
+        this.resultOfGame = resultOfGame;
+    }
+
+    public void changeActiveGameShutdown() {
+        this.isActiveGame = false;
+    }
+
+    public String getWordWithGuessedLetters() {
+        StringBuilder wordBuilder = new StringBuilder();
+        for (int i = 0; i < wordToGuess.word().length(); i++) {
+            if (wordCollectorModel.getLetters().contains(wordToGuess.word().charAt(i))) {
+                wordBuilder.append(wordToGuess.word().charAt(i));
+            } else {
+                wordBuilder.append('_');
+            }
+        }
+        return wordBuilder.toString();
+    }
+
+    public String getStringAboutAllCommands() {
+        return CommandInGameEnum.getStringAboutAllCommands();
+    }
+}
+
