a9d585d2981bec891eaccebd3c774c531d805e40
alexgureev <amgureev@edu.hse.ru>
2024-09-25 14:07:27 +0300

    feat: delete function input in StartMenu

--- src/main/java/backend/academy/hangman/Model/StartMenu.java
+++ src/main/java/backend/academy/hangman/Model/StartMenu.java
@@ -1,22 +1,9 @@
 package backend.academy.hangman.Model;
 
-import java.util.Scanner;
 import lombok.NoArgsConstructor;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 @NoArgsConstructor
 public class StartMenu {
-    private static final Logger LOGGER = LogManager.getLogger(StartMenu.class);
-
-    public int input() {
-        Scanner scanner = new Scanner(System.in);
-        while (!scanner.hasNextInt()) {
-            LOGGER.error("Error: enter an integer [1-3]");
-            scanner.next();
-        }
-        return scanner.nextInt();
-    }
 
     public void exit() {
         System.exit(0);
