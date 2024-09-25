8e705ab2c602bb54dcde59a743ca4038ff4cd89b
alexgureev <amgureev@edu.hse.ru>
2024-09-25 14:04:12 +0300

    feat: added new functions in GameView:
    printLastValidatorError;
    printListOfLettersUsed;
    printGallows;
    printGameLineWithDashes;
    printAllCommands;

--- src/main/java/backend/academy/hangman/View/GameView.java
+++ src/main/java/backend/academy/hangman/View/GameView.java
@@ -1,9 +1,13 @@
 package backend.academy.hangman.View;
 
 import backend.academy.hangman.Entity.WordEntity;
+import backend.academy.hangman.Model.HangmanStagesModel;
+import java.util.List;
+import lombok.NoArgsConstructor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+@NoArgsConstructor
 public class GameView {
     private static final Logger LOGGER = LogManager.getLogger(GameView.class);
 
@@ -39,7 +43,27 @@ public class GameView {
         LOGGER.info("Returning to the main menu...");
     }
 
-    public void printWarning() {
+    public void printWarningReuseOfTheLetter() {
         LOGGER.info("You already guessed this letter.");
+    }
+
+    public void printAllCommands(String gameCommands) {
+        LOGGER.info("Enter a letter or a command {}:", gameCommands);
+    }
+
+    public void printGameLineWithDashes(String gameLineWithDashes) {
+        LOGGER.info("Word: {}", gameLineWithDashes);
+    }
+
+    public void printGallows(HangmanStagesModel hangmanStageModel) {
+        LOGGER.info(hangmanStageModel.value());
+    }
+
+    public void printListOfLettersUsed(List<Character> listOfLettersUsed) {
+        LOGGER.info("Guessed letters: {}", listOfLettersUsed);
+    }
+
+    public void printLastValidatorError(String errorMessage) {
+        LOGGER.info(errorMessage);
     }
 }
