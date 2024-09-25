ea5e2f8fef114e535e336e5e83023394b741c839
alexgureev <amgureev@edu.hse.ru>
2024-09-25 14:01:36 +0300

    fix: change logic command list in CommandValidator

--- src/main/java/backend/academy/hangman/Model/CommandValidator.java
+++ src/main/java/backend/academy/hangman/Model/CommandValidator.java
@@ -1,14 +1,17 @@
 package backend.academy.hangman.Model;
 
+import backend.academy.hangman.Entity.CommandInGameEnum;
 import backend.academy.hangman.Entity.ValidatorErrorEntity;
 import backend.academy.hangman.Repository.Validator;
 import java.util.List;
 
 public class CommandValidator implements Validator {
-    private final static List<String> COMMAND_LIST = List.of("get hint", "exit", "menu");
+    private final static List<CommandInGameEnum> COMMAND_LIST = List.of(CommandInGameEnum.values());
 
-    public ValidatorErrorEntity isValid(String input) {
-        if (input.length() == 1 || COMMAND_LIST.contains(input)) {
+    public ValidatorErrorEntity isValid(String userInput) {
+        boolean isCommandValid = COMMAND_LIST.stream()
+            .anyMatch(command -> command.command.equalsIgnoreCase(userInput));
+        if (userInput.length() == 1 || isCommandValid) {
             return null;
         }
         return new ValidatorErrorEntity("Error: entered the wrong command.");
